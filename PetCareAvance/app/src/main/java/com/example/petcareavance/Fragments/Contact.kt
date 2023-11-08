ea8779a0d0fecc70d22293867ae9688883d805e5
Yamathe5-laptop <manuelsegmar@gmail.com>
2023-11-08 11:16:49 -0500

    ADD: soporte & publicar servicio

--- PetCareAvance/app/src/main/java/com/example/petcareavance/Fragments/Contact.kt
+++ PetCareAvance/app/src/main/java/com/example/petcareavance/Fragments/Contact.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.petcareavance.R
 
-class confirmReclamo: Fragment() {
+class Contact:Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_confirmrese, container, false)
+        val view = inflater.inflate(R.layout.fragment_contact, container, false)
 
-
-        val btnIrAProceso2 = view.findViewById<ImageButton>(R.id.imageButton2)
+        val btnIrAProceso = view.findViewById<Button>(R.id.button4)
+        btnIrAProceso.setOnClickListener {
+            avanzar2()
+        }
+        val btnIrAProceso2 = view.findViewById<ImageButton>(R.id.imageButton4)
         btnIrAProceso2.setOnClickListener {
             retroceder()
         }
@@ -24,7 +29,13 @@ class confirmReclamo: Fragment() {
         return view
     }
 
-
+    fun avanzar2() {
+        val procesoPublicacionFragment = confirmReclamo()
+        val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+        transaction.replace(R.id.fragment_container, procesoPublicacionFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 
     fun retroceder() {
         parentFragmentManager.popBackStack()
